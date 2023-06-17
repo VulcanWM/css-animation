c1d0261d806e7d1f5dbc8d283200402dd10ef47f
VulcanWM <logmedha09@gmail.com>
2023-06-17 21:26:09 +0100

    added a loop attribute to the animation

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var frame = 0;
+var loop = true;
 
 function changeFrame() {
     frame += 1;
+    console.log(frame)
     let nextFrame = document.getElementById(`frame${frame + 1}`)
     if (nextFrame != null){
         let currentFrame = document.getElementById(`frame${frame}`)
         currentFrame.style.display = "none"
         nextFrame.style.display = "block"
+    } else {
+        if (loop == true){
+            let currentFrame = document.getElementById(`frame${frame}`)
+            currentFrame.style.display = "none"
+            frame = 0;
+            document.getElementById("frame1").style.display = "block"
+        }
     }
-    let currentFrame = document.getElementById(`frame${frame}`)
 
 }
 
