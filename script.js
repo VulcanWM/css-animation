2e2d07af25ea1fa25b82992a34bec21b97cbea96
VulcanWM <logmedha09@gmail.com>
2023-06-18 19:17:55 +0100

    removed frame+1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var frame = 0;
+var frame = 1;
 var seconds = 1;
 var loop = true;
 var play = true;
@@ -10,17 +10,17 @@ loop_checkbox.checked = loop;
 function changeFrame() {
     if (play == true){
         frame += 1;
-        if (frame < numberOfFrames){
-            let nextFrame = document.getElementById(`frame${frame + 1}`)
-            let currentFrame = document.getElementById(`frame${frame}`)
+        if (frame <= numberOfFrames){
+            let nextFrame = document.getElementById(`frame${frame}`)
+            let currentFrame = document.getElementById(`frame${frame-1}`)
             currentFrame.style.display = "none"
             nextFrame.style.display = "block"
-            document.getElementById("frameslider").value = frame + 1;
+            document.getElementById("frameslider").value = frame;
         } else {
             if (loop == true){
                 let currentFrame = document.getElementById(`frame${numberOfFrames}`)
                 currentFrame.style.display = "none"
-                frame = 0;
+                frame = 1;
                 document.getElementById("frame1").style.display = "block"
                 document.getElementById("frameslider").value = 1;
             }
@@ -47,13 +47,12 @@ loop_checkbox.addEventListener('change', (event) => {
     }
 })
 
-function myFunction(val) {
+function changeSlider(val) {
     play_function()
-    console.log(frame);
     let oldFrame = frame;
-    document.getElementById(`frame${oldFrame + 1}`).style.display = "none"
-    frame = val-1;
-    document.getElementById(`frame${frame + 1}`).style.display = "block"
+    document.getElementById(`frame${oldFrame}`).style.display = "none"
+    frame = val;
+    document.getElementById(`frame${frame}`).style.display = "block"
     play_function()
 }
 
